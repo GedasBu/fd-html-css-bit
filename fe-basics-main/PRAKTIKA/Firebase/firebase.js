d4d56ccbe5509eb1039919d6bf2fc159833f84a4
Gediminas Butkus <gedcka@gmail.com>
2021-09-30 19:40:01 +0300

    ttt

---
+++ fe-basics-main/PRAKTIKA/Firebase/firebase.js
@@ -0,0 +1,11 @@
+import {initializeApp} from 'firebase/app';
+const firebaseApp = initializeApp(
+    {
+        apiKey: "AIzaSyCASNSKbMie9QxCNdpYw-7_nBxsmjMY2Nk",
+        authDomain: "bandymas-d77fa.firebaseapp.com",
+        projectId: "bandymas-d77fa",
+        storageBucket: "bandymas-d77fa.appspot.com",
+        messagingSenderId: "458065817602",
+        appId: "1:458065817602:web:9d462d68392b83a5a2371b",
+        measurementId: "G-TJG64PR41D"
+      });
---
+++ fe-basics-main/Slovikyne/README.md
@@ -0,0 +1,2 @@
+# slovikyne-rework
+
